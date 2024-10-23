833b1b6634b21b382da6e8ffb38aedf738f083b1
Cayo Cesar <cayocesar652@gmail.com>
2024-10-23 15:10:28 -0300

    Adicionar lógica para geração de chaves

--- generator_keys.py
+++ generator_keys.py
@@ -1,14 +1,20 @@
-import os
-import tkinter
-import tkinter.filedialog
-import tkinter.simpledialog
-import binascii
-import base64
-from Crypto.Cipher import AES
-from Crypto.Random import get_random_bytes
-from Crypto.PublicKey import RSA
-from Crypto.Cipher import PKCS1_OAEP
+import tkinter as tk
+from tkinter import ttk
 
 def keygen_menu():
+    root = tk.Tk()
+    root.title("Geração de Chaves")
+    root.geometry("300x300")
+
+    def exit_program():
+        root.quit()
+
+    def generate_keys():
+        pass
+
+    style = ttk.Style()
+
+    style.configure("TButton", font=("Arial", 12), padding=10)
+    style.configure("TLabel", font=("Arial", 16), padding=10)
 
     return
