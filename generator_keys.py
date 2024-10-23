d1fc753b77af125c7f005c49b7992437cd6d2d04
Cayo Cesar <cayocesar652@gmail.com>
2024-10-23 15:07:32 -0300

    Adicionar lógica para geração de chaves

--- generator_keys.py
+++ generator_keys.py
@@ -1,2 +1,14 @@
+import os
+import tkinter
+import tkinter.filedialog
+import tkinter.simpledialog
+import binascii
+import base64
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
+
 def keygen_menu():
+
     return
