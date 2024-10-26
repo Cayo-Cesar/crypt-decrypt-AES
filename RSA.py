0336c878136836bcc2834242c55138367501ebc2
Cayo Cesar <cayocesar652@gmail.com>
2024-10-26 10:42:17 -0300

    Versao final

--- RSA.py
+++ RSA.py
@@ -15,35 +15,28 @@ def rsa_menu():
 
     def sign_rsa():
         try:
-        # Escolha do arquivo de entrada
             file_to_sign = filedialog.askopenfilename(title="Selecione o arquivo a ser assinado")
             if not file_to_sign:
-                raise FileNotFoundError("Arquivo a ser assinado não foi selecion.")
+                raise FileNotFoundError("Arquivo a ser assinado não foi selecionado.")
 
-        # Entrada do caminho da chave privada
-            private_key_file = simpledialog.askstring("Chave Privada", "Digite o caminho do arquivo com a chave privada:")
+            private_key_file = filedialog.askopenfilename(title="Selecione o arquivo com a chave privada (.pem):")
             if not private_key_file:
-                raise FileNotFoundError("Caminho do arquivo com a chave privada não foi fornecido.")
+                raise FileNotFoundError("Arquivo com a chave privada não foi selecionado.")
 
-        # Seleção da versão do SHA-2
             sha_version = simpledialog.askstring("Versão do SHA-2", "Escolha a versão do SHA-2 (256, 384, 512):")
             if sha_version not in ["256", "384", "512"]:
                 raise ValueError("Versão inválida do SHA-2. Escolha entre 256, 384 ou 512.")
 
-        # Escolha do formato de saída (Hex ou Base64)
             output_format = simpledialog.askstring("Formato de Saída", "Escolha o formato de saída (Hex ou Base64):").upper()
             if output_format not in ["HEX", "BASE64"]:
                 raise ValueError("Formato de saída inválido. Escolha entre Hex ou Base64.")
 
-        # Leitura da chave privada
             with open(private_key_file, "rb") as f_priv:
                 private_key = RSA.import_key(f_priv.read())
 
-            # Leitura do arquivo a ser assinado
             with open(file_to_sign, "rb") as f_file:
                 message = f_file.read()
 
-        # Escolha do algoritmo de hash SHA-2
             if sha_version == "256":
                 h = SHA256.new(message)
             elif sha_version == "384":
@@ -51,60 +44,56 @@ def rsa_menu():
             elif sha_version == "512":
                 h = SHA512.new(message)
 
-        # Geração da assinatura
             signature = pkcs1_15.new(private_key).sign(h)
 
-        # Conversão da assinatura para o formato desejado
             if output_format == "HEX":
                 signature_out = binascii.hexlify(signature).decode()
+                filename = "assinatura_hex.txt"
             else:
                 signature_out = base64.b64encode(signature).decode()
+                filename = "assinatura_base64.txt"
 
-        # Salvando a assinatura em um arquivo
-            current_directory = os.getcwd()
-            with open(os.path.join(current_directory, "assinatura.txt"), "w") as f_sign:
+            with open(filename, "w") as f_sign:
                 f_sign.write(signature_out)
 
-            messagebox.showinfo("Sucesso", "Assinatura gerada e salva com sucesso!")
+            messagebox.showinfo("Sucesso", f"Assinatura gerada e salva em {filename} com sucesso!")
 
         except Exception as e:
             messagebox.showerror("Erro", str(e))
 
     def verify_rsa_signature():
         try:
-        # Entrada do arquivo com a assinatura
             signature_file = filedialog.askopenfilename(title="Selecione o arquivo com a assinatura")
             if not signature_file:
                 raise FileNotFoundError("Arquivo com a assinatura não foi selecionado.")
 
-        #    Entrada do arquivo em claro
             file_to_verify = filedialog.askopenfilename(title="Selecione o arquivo a ser verificado")
             if not file_to_verify:
                 raise FileNotFoundError("Arquivo a ser verificado não foi selecionado.")
 
-        # Entrada do caminho da chave pública
-            public_key_file = simpledialog.askstring("Chave Pública", "Digite o caminho do arquivo com a chave pública:")
+            public_key_file = filedialog.askopenfilename(title="Selecione o arquivo com a chave pública (.pem):")
             if not public_key_file:
-                raise FileNotFoundError("Caminho do arquivo com a chave pública não foi fornecido.")
+                raise FileNotFoundError("Arquivo com a chave pública não foi selecionado.")
 
-        # Seleção da versão do SHA-2
             sha_version = simpledialog.askstring("Versão do SHA-2", "Escolha a versão do SHA-2 (256, 384, 512):")
             if sha_version not in ["256", "384", "512"]:
                 raise ValueError("Versão inválida do SHA-2. Escolha entre 256, 384 ou 512.")
 
-        # Leitura da chave pública
+            output_format = simpledialog.askstring("Formato da Assinatura", "Formato da assinatura (Hex ou Base64):").upper()
+            if output_format not in ["HEX", "BASE64"]:
+                raise ValueError("Formato de assinatura inválido. Escolha entre Hex ou Base64.")
+
             with open(public_key_file, "rb") as f_pub:
                 public_key = RSA.import_key(f_pub.read())
 
-        # Leitura do arquivo com a assinatura
-            with open(signature_file, "rb") as f_sign:
-                signature = f_sign.read()
+            with open(signature_file, "r") as f_sign:
+                signature_encoded = f_sign.read()
 
-        # Leitura do arquivo a ser verificado
+            signature = binascii.unhexlify(signature_encoded) if output_format == "HEX" else base64.b64decode(signature_encoded)
+
             with open(file_to_verify, "rb") as f_file:
                 message = f_file.read()
 
-        # Escolha do algoritmo de hash SHA-2
             if sha_version == "256":
                 h = SHA256.new(message)
             elif sha_version == "384":
@@ -112,16 +101,14 @@ def rsa_menu():
             elif sha_version == "512":
                 h = SHA512.new(message)
 
-        # Verificação da assinatura
-            try:
-                pkcs1_15.new(public_key).verify(h, signature)
-                messagebox.showinfo("Sucesso", "A assinatura é válida!")
-            except (ValueError, TypeError):
-                messagebox.showerror("Erro", "A assinatura é inválida!")
+            pkcs1_15.new(public_key).verify(h, signature)
+            messagebox.showinfo("Sucesso", "A assinatura é válida!")
 
+        except (ValueError, TypeError):
+            messagebox.showerror("Erro", "A assinatura é inválida!")
         except Exception as e:
             messagebox.showerror("Erro", str(e))
-
+            
     style = ttk.Style()
 
     label_title = ttk.Label(window, text="Assinatura RSA", anchor="center")
@@ -135,6 +122,7 @@ def rsa_menu():
 
     button_verify = ttk.Button(window, text="Verificar Assinatura", command=verify_rsa_signature, width=30)
     button_verify.pack(pady=10)
+
 
     #window.mainloop()
 
